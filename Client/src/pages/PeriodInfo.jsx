c839645f1db58af0de71bcb2a8b38c99cea8faf3
Oluwatoyin Oredein <104014691+codinBabe@users.noreply.github.com>
2024-05-01 14:13:51 -0900

    Update PeriodInfo.jsx

--- Client/src/pages/PeriodInfo.jsx
+++ Client/src/pages/PeriodInfo.jsx
@@ -26,7 +26,7 @@ export default function PeriodInfo() {
   }, []);
 
   function handleRecalculate() {
-    window.location = "tracker/calculate";
+    window.location = "/tracker/calculate";
   }
   return (
     <>
